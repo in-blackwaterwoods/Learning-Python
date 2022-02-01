210ede42fb1b95adabb6edec89620d677676131a
Kaylee K <hj.o3os2@gmail.com>
2022-02-01 19:55:49 +0900

    숫자 문자열 반환

--- str.py
+++ str.py
@@ -3,3 +3,6 @@ output_a = str(52)
 output_b = str(52.273)
 print(type(output_a), output_a)
 print(type(output_b), output_b)
+
+#숫자(float, int) str 붙여서 문자열 만들면 +로 문자랑 붙이기 가능!
+#예시: print("사랑하는"+str(100)+"점")
