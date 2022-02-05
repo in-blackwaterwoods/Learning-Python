a79c4133f01ccb69cbe2d54e1996d681f1b8596a
Kaylee K <hj.o3os2@gmail.com>
2022-02-05 23:31:26 +0900

    upper와 trim

---
+++ upper.py
@@ -0,0 +1,17 @@
+a = "Hello World Python Programming"
+print(a.upper())
+print(a.lower())
+
+input_a = """
+    안녕하세요
+문자열의 함수를 알아봅니다
+"""
+
+print(input_a)
+
+print(input_a.strip())
+
+print("---------------------------")
+
+print("TrainA10".isalnum())
+print("10".isdigit())
