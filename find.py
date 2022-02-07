390236f307ebe4c19d92f764e603a12713debc39
Kaylee K <hj.o3os2@gmail.com>
2022-02-07 23:55:58 +0900

    find() 함수

---
+++ find.py
@@ -0,0 +1,11 @@
+output_a = "안녕안녕하세요".find("안녕")
+print(output_a)
+
+output_b = "안녕안녕하세요".rfind("안녕")
+print(output_b)
+
+print("안녕" in "안녕하세요")
+print("잘자" in "안녕하세요")
+
+a = " 10 20 30 40 50".split(" ")
+print(a)
