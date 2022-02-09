2e596e368e37b59fcdc22f22f56dbc4bd21f8752
Kaylee K <hj.o3os2@gmail.com>
2022-02-09 21:48:55 +0900

    02-4테스트

---
+++ 02-4_test.py
@@ -0,0 +1,21 @@
+a = (input("> 1번째 숫자: "))
+b = (input("> 2번째 숫자: "))
+print()
+
+print("{} + {} = {}".format(int(a), int(b), int(a+b)))
+
+
+##04 
+
+string = "hello"
+
+#string.upper()를 실행하고, string 출력하기 
+string.upper()
+print("A 지점:", string)
+
+# string.upper() 실행하기 
+print("B 지점:", string.upper())
+
+##결과 예측 
+#A  지점: hello 
+#B 지점: HELLO 
